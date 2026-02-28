be23884ee772207f545ed73376207f3e9778f081
Arturo Cruz <foro7.arturocruz@gmail.com>
2026-02-28 16:40:06 -0600

    Agrega fotos bodas plata

--- photos_list.js
+++ photos_list.js
@@ -1,7 +1,19 @@
 // Lista de fotos generada automáticamente
-// Ejecuta generate_photo_list.py para regenerar este archivo
+// Total de fotos: 2
+// Generado: 2026-02-28 16:39:57
 
-const photos = [];
+const photos = [
+    {
+        "name": "DJI_20260228_162033_665",
+        "path": "imagenes/DJI_20260228_162033_665.webp",
+        "filename": "DJI_20260228_162033_665.webp"
+    },
+    {
+        "name": "DJI_20260228_162037_491",
+        "path": "imagenes/DJI_20260228_162037_491.webp",
+        "filename": "DJI_20260228_162037_491.webp"
+    }
+];
 
 window.addEventListener('DOMContentLoaded', function() {
     console.log(`Cargadas ${photos.length} fotos`);
