03ed4089a1d2852409b7363a37de87d2445c08a8
Arturo Cruz <foro7.arturocruz@gmail.com>
2026-02-28 16:40:49 -0600

    Actualiza lista de fotos

--- photos_list.js
+++ photos_list.js
@@ -1,6 +1,6 @@
 // Lista de fotos generada automáticamente
-// Total de fotos: 2
-// Generado: 2026-02-28 16:39:57
+// Total de fotos: 7
+// Generado: 2026-02-28 16:40:49
 
 const photos = [
     {
@@ -12,6 +12,31 @@ const photos = [
         "name": "DJI_20260228_162037_491",
         "path": "imagenes/DJI_20260228_162037_491.webp",
         "filename": "DJI_20260228_162037_491.webp"
+    },
+    {
+        "name": "DJI_20260228_162041_044",
+        "path": "imagenes/DJI_20260228_162041_044.webp",
+        "filename": "DJI_20260228_162041_044.webp"
+    },
+    {
+        "name": "DJI_20260228_162111_556",
+        "path": "imagenes/DJI_20260228_162111_556.webp",
+        "filename": "DJI_20260228_162111_556.webp"
+    },
+    {
+        "name": "DJI_20260228_162116_413",
+        "path": "imagenes/DJI_20260228_162116_413.webp",
+        "filename": "DJI_20260228_162116_413.webp"
+    },
+    {
+        "name": "DJI_20260228_162120_675",
+        "path": "imagenes/DJI_20260228_162120_675.webp",
+        "filename": "DJI_20260228_162120_675.webp"
+    },
+    {
+        "name": "DJI_20260228_162124_409",
+        "path": "imagenes/DJI_20260228_162124_409.webp",
+        "filename": "DJI_20260228_162124_409.webp"
     }
 ];
 
